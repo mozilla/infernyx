f32cf8565b3a649c137e64ab4419886b6c32d896
tspurway <tspurway@gmail.com>
2014-11-13 13:44:36 -0500

    fixing setup.py requirements
    versi

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='infernyx',
-    version='0.1.23',
+    version='0.1.24',
     packages=['infernyx'],
     url='',
     license='',
     author='tspurway',
     author_email='tspurway@mozilla.com',
     description='Inferno rules for Tiles Project',
-    requires=['inferno', 'boto', 'geoip2', 'ua_parser', 'statsd'],
+    install_requires=['inferno', 'boto', 'geoip2', 'ua_parser', 'statsd'],
     zip_safe=False,
 )
