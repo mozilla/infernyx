b5145e1086c2fc8c75c4bc5d6f821f29dcc92479
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2015-02-09 11:13:20 -0800

    Cacth parse exceptions

--- tools/xcat_to_csv.py
+++ tools/xcat_to_csv.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 import sys
 import re
+import logging
 
 def readLines():
   line = sys.stdin.readline()
   while (len(line) > 0):
-    o1, o2 = re.split('\t', line)
-    items = eval(o1) + eval(o2)
-    print ",".join(str(item) for item in items)
+    try:
+      o1, o2 = re.split('\t', line)
+      items = eval(o1) + eval(o2)
+      print ",".join(str(item) for item in items)
+    except Exception as e:
+      logging.error("Error processing line '%s': %s" % (line, e))
+
     line = sys.stdin.readline()
 
 if __name__ == '__main__':
