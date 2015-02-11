868fc05894087fc0b07a588765e5c62fff3756b5
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2015-02-11 09:11:01 -0800

    Added debug to histogram and chnaged key numbering

--- tools/histogram.py
+++ tools/histogram.py
@@ -5,6 +5,7 @@ import logging
 from optparse import OptionParser
 
 stats = {'total': 0, 'keys':{}}
+options = None
 
 def addKeyValue(keysIndex, valueIndex, lineItems):
   ## make a key
@@ -29,20 +30,25 @@ def readLines(keysIndex, valueIndex):
     line = sys.stdin.readline()
 
 def printStats():
-  print "TOTAL: %d" % stats["total"]
+  if (options.debug):
+    print "TOTAL: %d" % stats["total"]
   for key in sorted(stats["keys"].iterkeys()):
-    print "%s %d %.2f" % (key, stats["keys"][key], stats["keys"][key] * 100 / stats["total"])
+    if (options.debug):
+      print "%s,%d,%.2f" % (key, stats["keys"][key], stats["keys"][key] * 100 / stats["total"])
+    else:
+      print "%s,%d" % (key, stats["keys"][key])
 
 def readArgs():
   parser = OptionParser()
   parser.add_option("-k", "--key", dest="keys", help="coma separated key fields")
   parser.add_option("-v", "--value", dest="value", help="value field")
+  parser.add_option("-d", "--debug", action="store_true", default=False, dest="debug", help="show extra verbose info")
   return parser.parse_args()
 
 if __name__ == '__main__':
   (options, args) = readArgs()
-  keys = [int(i) for i in re.split(',', options.keys)]
-  value = int(options.value)
+  keys = [int(i) - 1 for i in re.split(',', options.keys)]
+  value = int(options.value) - 1
   readLines(keys, value)
   printStats()
 
