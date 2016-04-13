349aca742856b16299157aaed45a4479cef5074c
Nan Jiang <njiang028@gmail.com>
2016-04-13 14:13:55 -0400

    Include expire_ddfs_data.py into shipping scripts

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='tspurway@mozilla.com',
     description='Inferno rules for Tiles Project',
     install_requires=['psycopg2', 'inferno', 'boto', 'geoip2', 'ua_parser', 'statsd', 'datadog'],
-    scripts=['scripts/csdash.py'],
+    scripts=['scripts/csdash.py', 'scripts/expire_ddfs_data.py'],
     zip_safe=False,
 )
