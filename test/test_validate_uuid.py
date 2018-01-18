285a9641611b3fb41a2e4d4a32ab25feb78c80b5
Nan Jiang <njiang028@gmail.com>
2018-01-18 15:33:23 -0500

    Add a special case for uuid validation

--- test/test_validate_uuid.py
+++ test/test_validate_uuid.py
@@ -9,7 +9,8 @@ class TestRuleHelperFunctions(unittest.TestCase):
     def testInvalidateUuid4(self):
         parts = {
             "client_id": "6954f539-0c58-4b2f-afdf-d70c7d4d5e0c",
-            "session_id": "{5cbc6ab2-b3b8-478a-8d79-159dcf3021a7}"
+            "session_id": "{5cbc6ab2-b3b8-478a-8d79-159dcf3021a7}",
+            "special_id": "n/a"
         }
         new_parts = validate_uuid4(parts, {}, ["client_id", "session_id"]).next()
         self.assertDictEqual(parts, new_parts)
